0bd3ddc1338e46eedfcbf4ecc0473df64d447879
DMuld <mulder.z.devin@gmail.com>
2026-03-10 21:51:06 -0500

    Updated for nvim .11

--- init.lua
+++ init.lua
@@ -342,8 +342,9 @@ require('lazy').setup({
         ts_ls = {},
         svelte = {},
         jdtls = {},
-
         docker_compose_language_service = {},
+        gleam = {},
+
         -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
         lua_ls = {
           -- cmd = { ... },
@@ -367,9 +368,19 @@ require('lazy').setup({
       -- other tools, you can run
       --    :Mason
       local ensure_installed = vim.tbl_keys(servers or {})
+
+      -- Mason does not have a "gleam" package; we use system `gleam lsp`
+      for i = #ensure_installed, 1, -1 do
+        if ensure_installed[i] == 'gleam' then
+          table.remove(ensure_installed, i)
+        end
+      end
+
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
       })
+
+      local lspconfig = require('lspconfig')
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
@@ -380,11 +391,10 @@ require('lazy').setup({
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            vim.lsp.config(server_name).setup(server)
+            lspconfig[server_name].setup(server)
           end,
         },
       }
-      vim.lsp.enable('gleam')
       -- NOTE: Allows me to install more custom LSPs
       require("mason").setup({
         registries = {
